9d05a92eb30f74e6f53423b8f6a645b6d1a44cda
NandhiniRam2005 <nandhini2005@outlook.com>
2024-11-19 22:00:37 -0700

    XML commented the Model classes and Network controller class

--- Snake/Snake.Client/Controller/NetworkController.cs
+++ Snake/Snake.Client/Controller/NetworkController.cs
@@ -1,4 +1,8 @@
-﻿using System.Numerics;
+﻿// <copyright file="NetworkConnection.cs" company="UofU-CS3500">
+// Copyright (c) 2024 UofU-CS3500. All rights reserved.
+// </copyright>
+
+using System.Numerics;
 using CS3500.Networking;
 using Microsoft.Extensions.Logging;
 using System.Xml.Linq;
@@ -8,15 +12,62 @@ using Microsoft.JSInterop;
 
 namespace Snake.Client.Controller;
 
+/// <summary>
+/// Author:    Joel Rodriguez,  Nandhini Ramanathan, and Professor Jim.
+/// Partner:   None
+/// Date:      November 22, 2024
+/// Course:    CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500 and [Joel Rodriguez and Nandhini Ramanathan] - This work may no
+///            be copied for use in Academic Coursework.
+///
+/// I, Joel Rodriguez and Nandhini Ramanathan, certify that I wrote this code from scratch and
+/// did not copy it in part or whole from another source.  All
+/// references used in the completion of the assignments are cited
+/// in my README file.
+///
+/// File Contents
+///     This class manages a network connection for a Snake game, handling communication 
+///     with a game server. It includes methods to connect to the server, receive updates, and send user input 
+///     commands. This class uses the NetworkConnection class to manage the underlying TCP connection and includes 
+///     methods to handle user input via key presses.
+/// </summary>
+
+///BLABLA
 public class NetworkController
 {
+	/// <summary>
+	/// The network connection to the game server, used to send and receive data.
+	/// </summary>
 	public NetworkConnection server = new(NullLogger.Instance);
+
+	/// <summary>
+	/// A string indicating the current status of the network connection.
+	/// </summary>
 	public string networkStatus = "Waiting For You to Connect";
+
+	/// <summary>
+	/// Keeps track of the frame number for network updates.
+	/// </summary>
 	private int frameNumberNetwork = 0;
+
+	/// <summary>
+	/// Stores the time when the connection was established.
+	/// </summary>
 	private DateTime ConnectTime = DateTime.Now;
+
+	/// <summary>
+	/// Represents the current direction of the player's snake.
+	/// </summary>
 	private String snakeDirection = "Up";
 
-
+	/// <summary>
+	/// Handles the network connection to the game server. Establishes a connection,
+	/// retrieves initial world information, and continuously receives updates from the server.
+	/// </summary>
+	/// <param name="world">The World object representing the game state.</param>
+	/// <param name="serverUrl">The URL of the server to connect to.</param>
+	/// <param name="port">The port number used for the connection.</param>
+	/// <param name="name">The name of the snake (player).</param>
 	public async void HandleNetwork(World world, string serverUrl, int port, string name)
 	{
 
@@ -36,13 +87,16 @@ public class NetworkController
 		});
 
 		var worldJSON = "";
-		// Once the server has connected do all this good stuff.
+
+		// Once connected to the server, proceed with game initialization
 		if (server.IsConnected)
 		{
-			server.Send(name); // Sends the name of the snake.
+			server.Send(name);
 			world.WorldID = int.Parse(server.ReadLine());
-			world.Height = int.Parse(server.ReadLine()); // Maybe switch tryParse
+			world.Height = int.Parse(server.ReadLine()); // Maybe switch tryParse???
 			world.Width = world.Height;
+
+			// Start a background task to continuously receive updates from the server
 			await Task.Run(() =>
 			{
 				try
@@ -52,7 +106,8 @@ public class NetworkController
 
 						worldJSON = server.ReadLine();
 
-						lock (world) // make a copy in the lock and then do the work on that copy
+						// Lock the `world` object to safely update its state with the new dat
+						lock (world)
 						{
 							world.load(worldJSON);
 						}
@@ -68,6 +123,12 @@ public class NetworkController
 			});
 		}
 	}
+
+	/// <summary>
+	/// Handles key press events from the user, interpreting them as movement commands for the snake.
+	/// Sends the corresponding direction to the server if the snake is connected.
+	/// </summary>
+	/// <param name="keyEvent">The key event string representing the user's key press.</param>
 	[JSInvokable]
 	public void HandleKeyPress(String keyEvent)
 	{
@@ -75,8 +136,7 @@ public class NetworkController
 		{
 			return;
 		}
-		//Code for handing
-		//Send the key that was pressed to the server (says in assignment maybe?)
+
 		if (keyEvent == "ArrowUp" || keyEvent == "w" && snakeDirection != "DOWN")
 		{
 			server.Send(@"{""moving"":""up""}");
--- Snake/Snake.Client/Models/Snake.cs
+++ Snake/Snake.Client/Models/Snake.cs
@@ -1,28 +1,106 @@
-﻿using System.Text.Json.Serialization;
+﻿// <copyright file="NetworkConnection.cs" company="UofU-CS3500">
+// Copyright (c) 2024 UofU-CS3500. All rights reserved.
+// </copyright>
+
+using System.Text.Json.Serialization;
 
 namespace Snake.Client.Models;
 
+/// <summary>
+/// Author:    Joel Rodriguez,  Nandhini Ramanathan, and Professor Jim.
+/// Partner:   None
+/// Date:      November 22, 2024
+/// Course:    CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500 and [Joel Rodriguez and Nandhini Ramanathan] - This work may no
+///            be copied for use in Academic Coursework.
+///
+/// I, Joel Rodriguez and Nandhini Ramanathan, certify that I wrote this code from scratch and
+/// did not copy it in part or whole from another source.  All
+/// references used in the completion of the assignments are cited
+/// in my README file.
+///
+/// File Contents
+///     This class represents a player-controlled snake in the Snake game.
+///     It includes the snake's unique ID, name, position on the game grid (body and direction),
+///     game status (alive, dead, or disconnected), and score. It handles the representation of a snake's state, 
+///     including movement direction, whether it has died, and if it has joined or left the game.
+/// </summary>
+
+
+/// <summary>
+///   Represents a player-controlled snake in the Snake game, including its ID, name, position,
+///   score, status, and connection information.
+/// </summary>
 public class Snake
 {
+	/// <summary>
+	/// Gets or sets the unique ID of the snake.
+	/// </summary>
 	[JsonInclude]
 	public int snake { get; set; }
+
+	/// <summary>
+	/// Gets or sets the name of the snake/player.
+	/// </summary>
 	[JsonInclude]
 	public string name { get; set; }
+
+	/// <summary>
+	/// Gets or sets the body of the snake, represented as a list of points.
+	/// Each point indicates a part of the snake's body.
+	/// </summary>
 	[JsonInclude]
 	public List<Point2D> body {get; set;}
+
+	/// <summary>
+	/// Gets or sets the direction the snake is moving.
+	/// </summary>
 	[JsonInclude]
 	public Point2D dir { get; set;}
+
+	/// <summary>
+	/// Gets or sets the score of the snake.
+	/// </summary>
 	[JsonInclude]
 	public int score { get; set; }
+
+	/// <summary>
+	/// Gets or sets a value indicating whether the snake has died.
+	/// </summary>
 	[JsonInclude]
 	public bool died { get; set; }
+
+	/// <summary>
+	/// Gets or sets a value indicating whether the snake is currently alive.
+	/// </summary>
 	[JsonInclude]
 	public bool alive { get; set; }
+
+	/// <summary>
+	/// Gets or sets a value indicating whether the snake has disconnected.
+	/// </summary>
 	[JsonInclude]
 	public bool dc {  get; set; }
+
+	/// <summary>
+	/// Gets or sets a value indicating whether the snake has joined the game.
+	/// </summary>
 	[JsonInclude]
 	public bool join { get; set; }
-    public Snake(int snake, string name, List<Point2D> body, Point2D dir, int score, bool died, bool alive, bool dc, bool join)
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Snake"/> class.
+	/// </summary>
+	/// <param name="snake">The unique ID of the snake.</param>
+	/// <param name="name">The name of the snake or player.</param>
+	/// <param name="body">The body of the snake, represented as a list of points.</param>
+	/// <param name="dir">The direction the snake is moving.</param>
+	/// <param name="score">The score of the snake.</param>
+	/// <param name="died">Indicates whether the snake has died.</param>
+	/// <param name="alive">Indicates whether the snake is alive.</param>
+	/// <param name="dc">Indicates whether the snake has disconnected.</param>
+	/// <param name="join">Indicates whether the snake has joined the game.</param>
+	public Snake(int snake, string name, List<Point2D> body, Point2D dir, int score, bool died, bool alive, bool dc, bool join)
     {
         this.snake = snake;
         this.name = name;
